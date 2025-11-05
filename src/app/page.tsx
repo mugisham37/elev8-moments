0a10c66139abc57c36acbc97ba33c623124dd987
mugisham37 <mugisham333@gmail.com>
2025-11-05 11:01:58 +0200

    Added in the offering section on the landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import FruitSpecialtyBoards from '@/components/Landing/FruitSpecialtyBoards'
 import Marquee from '@/components/Landing/Marquee'
 import BoardBestiesBanner from '@/components/Landing/BoardBestiesBanner'
 import TestimonialsSection from '@/components/Layout/Testimonials'
+import Offering from '@/components/Landing/offering/offering'
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <WelcomeSection />
+      <Offering/>
       <FruitSpecialtyBoards />
       <Marquee text="Glam Haus Design Co." />
       <BoardBestiesBanner />
