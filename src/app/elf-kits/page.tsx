90966f05f19171dde8ec8f1cd26d06282d9636b6
mugisham37 <mugisham333@gmail.com>
2025-11-05 10:34:27 +0200

    Solved the errors on the elf page

--- src/app/elf-kits/page.tsx
+++ src/app/elf-kits/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Routes/Elf-Kit/Hero";
+import { HowItWorks } from "@/components/Routes/Elf-Kit/How it works/HowItWorks";
 
 export default function App() {
   return (
     <>
     <Hero/>
+    <HowItWorks/>
     </>
   );
 }
